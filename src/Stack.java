d620cf3630ff8bc57dea692d42a35c3f4940d910
Mark <mrvs025@gmail.com>
2022-01-02 12:08:36 +0200

    added a lot of functions

--- src/Stack.java
+++ src/Stack.java
@@ -86,24 +86,164 @@ public class Stack<T>
 		return stackString;
 	}
 	// ---------------------------------------------------------------------------------------
-
-	// Static Functions
 	// ---------------------------------------------------------------------------------------
-	public static boolean isSorted(Stack<Integer> stackToCheck)
-	{
-		Stack<Integer> temp = new Stack<Integer>(stackToCheck);
-		int top;
+	// ---------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------
+	//mark function
 
-		while (!temp.isEmpty())
-		{
-			top = temp.pop();
-			// if the stack isn't already empty then check for sort validation
-			if (!temp.isEmpty() && top < temp.top())
-			{
-				return false;
+
+	public Stack<Integer>backsort(){
+		Stack<Integer>a=this.copy();
+		return a.sort().reverse();
+	}
+	public int getLenth(){
+		Stack a = this.copy();
+		int i =0;
+		while (!a.isEmpty()){
+			i++;
+			a.pop();
+		}
+		return i;
+	}
+
+	public Stack<Integer> sort(){
+		Stack<Integer>temp = new Stack<>();
+		Stack<Integer>a = this.copy();
+		Stack<Integer>sorted=new Stack<Integer>();
+
+		while (sorted.getLenth()!=this.getLenth()) {
+			sorted.push(getmin(a));
+			while (a.top() != getmin(a)) {
+				temp.push(a.pop());
+			}
+			a.pop();
+			while (!temp.isEmpty()) {
+				a.push(temp.pop());
 			}
 		}
-		return true;
+		return sorted;
 	}
+	public Stack<Integer> deletenum(int num){
+		Stack neww = new Stack();
+		Stack<Integer>a=this.copy();
+		a=a.reverse();
+		while(!a.isEmpty()){
+			if(a.top()==num){
+				a.pop();
+			}
+			else{
+				neww.push(a.pop());
+			}
+		}
+		return neww;
+	}
+	public static int getmin(Stack<Integer>a){
+		Stack<Integer>b = a.copy();
+		int k=b.pop();
+		while(!b.isEmpty()){
+			k=Math.min(b.pop(), k);
+		}
+		return k;
+	}
+	public static int getmax(Stack<Integer>a){
+		Stack<Integer>b = a.copy();
+		int k=b.pop();
+		while(!b.isEmpty()){
+			k=Math.max(b.pop(), k);
+		}
+		return k;
+	}
+
+	public Stack copy()
+	{
+		Stack<T> h1=new Stack<>();
+		Stack<T> h2=new Stack<>();
+
+
+		while(!this.isEmpty())
+		{
+			h1.push(this.pop());
+		}
+		while(!h1.isEmpty())
+		{
+			this.push(h1.top());
+			h2.push(h1.pop());
+		}
+		return h2;
+	}
+	public Stack reverse(){
+		Stack<T>a=this.copy();
+		Stack<T>b=new Stack<>();
+		while(!a.isEmpty()){
+			b.push(a.pop());
+		}
+		return b;
+	}
+	public boolean issorted(){
+		return (equalsto(this.copy(), this.sort()));
+	}
+	public static boolean equalsto(Stack <Integer> a, Stack<Integer> b){
+		Stack<Integer> c=a.copy();
+		Stack<Integer> d=b.copy();
+		boolean flag = true;
+		if(c.getLenth()!=d.getLenth()){
+			return false;
+		}
+		while (!c.isEmpty()&&!d.isEmpty()){
+			if (c.pop()!=d.pop()){
+				flag = false;
+			}
+		}
+		return flag;
+	}
+	public static boolean containssamenumbers(Stack<Integer> a, Stack<Integer> b){
+		Stack<Integer>c=a.copy();
+		Stack<Integer>d=b.copy();
+		return equalsto(c.sort(),d.sort());
+	}
+	public static Stack merge(Stack<Integer> g, Stack<Integer> f) {
+		Stack<Integer>a = g.copy();
+		Stack<Integer>b=f.copy();
+		Stack<Integer> c = new Stack<Integer>();
+		while (!a.isEmpty()&&!b.isEmpty()) {
+			int anum = a.top();
+			int bnum = b.top();
+			if (anum >= bnum) {
+				c.push(anum);
+				a.pop();
+			} else {
+				c.push(bnum);
+				b.pop();
+			}
+		}
+		if (a.isEmpty()){
+			while (!b.isEmpty()){
+				c.push(b.pop());
+			}
+
+		}
+		if (b.isEmpty()){
+			while (!a.isEmpty()){
+				c.push(a.pop());
+			}
+
+		}
+		return c;
+	}
+	public boolean isin(T x){
+		Stack<T> a= this.copy();
+		while(!a.isEmpty()){
+			if(a.pop()==x){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+	//
 	// ---------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------
+	// ---------------------------------------------------------------------------------------
+
 }
