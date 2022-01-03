8bb31c6943452cea8414a77c3edfaf957f1c6097
programmer1024r <reempro112@gmail.com>
2022-01-03 15:13:25 +0200

    added all needed function for the bagrut test

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,29 @@ public class Main
 {
     public static void main(String[] args)
     {
-        Stack<Integer> stack = new Stack<Integer>();
-        stack.push(1);
-        stack.push(2);
-        stack.push(3);
-        stack.push(4);
-        stack.push(101);
-        System.out.println(stack.toString());
-        System.out.println(Stack.isSorted(stack));
-        System.out.println(stack.length());
+        Stack<Integer> stack1 = new Stack<Integer>();
+        Stack<Integer> stack2 = new Stack<Integer>();
+        // stack 1
+        stack1.push(1);
+        stack1.push(2);
+        stack1.push(3);
+
+        // stack 2
+        stack2.push(1);
+        stack2.push(2);
+        stack2.push(3);
+        /* stack2.push(-1);
+        stack2.push(2);
+        stack2.push(4);
+        stack2.push(5);*/
+
+
+        System.out.println(Stack.mergeStacks(stack1, stack2).toString());
+        System.out.println(stack1.toString());
+        System.out.println(stack2.toString());
+        System.out.println(Stack.valueExists(stack2, 0));
+        System.out.println(Stack.shiftStack(stack2));
+        System.out.println(Stack.areEqual(stack1, stack2));
 
     }
 }
--- src/Stack.java
+++ src/Stack.java
@@ -3,6 +3,7 @@ public class Stack<T>
 {
 	private Node<T> head;
 	private static final String TOP = " __top__ \n";
+	private static final int INVALID_MIN = -1;
 	// CTOR
 	// ---------------------------------------------------------------------------------------
 	public Stack()
@@ -56,7 +57,6 @@ public class Stack<T>
 		return head == null;
 	}
 
-
 	public int length()
 	{
 		Stack<T> temp = new Stack<>(this);
@@ -86,9 +86,118 @@ public class Stack<T>
 		return stackString;
 	}
 	// ---------------------------------------------------------------------------------------
-
-	// Static Functions
+	// Advance static Methods
 	// ---------------------------------------------------------------------------------------
+	public static Stack<Integer> sortStack(Stack<Integer> stackToSort)
+	{
+		Stack<Integer> temp = new Stack<>(stackToSort);
+		Stack<Integer> sortedStack = new Stack<>();
+
+		while (!temp.isEmpty())
+		{
+			Stack.sinkNode(sortedStack, temp.pop());
+		}
+
+		return sortedStack;
+	}
+
+	// sink a node to its right value in a sorted stack
+	public static void sinkNode(Stack<Integer> stackToSink, int value)
+	{
+		Stack<Integer> temp = new Stack<>();
+		int currVal;
+
+		// go through the stack and pop its values to temp till we find where value needs to be
+		while (!stackToSink.isEmpty())
+		{
+			currVal = stackToSink.top();
+			if (currVal > value)
+			{
+				temp.push(stackToSink.pop());
+			}
+			// Found place
+			else
+			{
+				break;
+			}
+		}
+		// add the new value to its rightful play in the sorted stack
+		stackToSink.push(value);
+
+		// fill the stack with the temp stack values.
+		while (!temp.isEmpty())
+		{
+			stackToSink.push(temp.pop());
+		}
+	}
+
+
+	public static Stack<Integer> mergeStacks(Stack<Integer> stack1, Stack<Integer> stack2)
+	{
+		Stack<Integer> mergedStack = new Stack<>(stack1);
+		Stack<Integer> tempSortedS2 = new Stack<>(stack2);
+
+		while (!tempSortedS2.isEmpty())
+		{
+			Stack.sinkNode(mergedStack, tempSortedS2.pop());
+		}
+
+		return mergedStack;
+	}
+	public static Stack<Integer> shiftStack(Stack<Integer> stackToShift)
+	{
+		if (stackToShift.isEmpty()) return  stackToShift;
+
+		Stack<Integer> shiftedStack = new Stack<>(stackToShift);
+		Stack<Integer> temp = new Stack<>();
+		int lastValue;
+
+		// save all values in a temp stack
+		while (!shiftedStack.isEmpty())
+		{
+			temp.push(shiftedStack.pop());
+		}
+		// save the last value & and add back all the values
+		lastValue = temp.pop();
+		while (!temp.isEmpty())
+		{
+			shiftedStack.push(temp.pop());
+		}
+		// add to the top the last value.
+		shiftedStack.push(lastValue);
+
+		return shiftedStack;
+	}
+
+	public static boolean valueExists(Stack<Integer> stackToSearch, int value)
+	{
+		Stack<Integer> temp = new Stack<>(stackToSearch);
+		while(!temp.isEmpty())
+		{
+			if (value == temp.pop())
+				return true;
+		}
+		return false;
+	}
+
+	public static int findMin(Stack<Integer> stackToSearch)
+	{
+		Stack<Integer> temp = new Stack<>(stackToSearch);
+
+		int current, min = (stackToSearch.isEmpty()) ? INVALID_MIN : temp.top();
+
+		while (!temp.isEmpty())
+		{
+			current = temp.pop();
+			if (current < min)
+			{
+				min = current;
+			}
+		}
+
+		return min;
+	}
+
 	public static boolean isSorted(Stack<Integer> stackToCheck)
 	{
 		Stack<Integer> temp = new Stack<Integer>(stackToCheck);
@@ -102,6 +211,23 @@ public class Stack<T>
 			{
 				return false;
 			}
+		}
+		return true;
+	}
+
+
+	public static boolean areEqual(Stack<Integer> stack1, Stack<Integer> stack2)
+	{
+		Stack<Integer> temp1 = new Stack<Integer>(stack1);
+		Stack<Integer> temp2 = new Stack<Integer>(stack2);
+		// if one of the stacks is empty
+		if (!temp1.isEmpty() ^ !temp2.isEmpty())
+			return false;
+
+		while (!temp1.isEmpty() && !temp2.isEmpty())
+		{
+			if (temp1.pop() != temp2.pop())
+				return false;
 		}
 		return true;
 	}
